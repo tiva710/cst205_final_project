589d8820c785182536910e7012d12cf4118afee6
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2024-11-14 21:38:29 -0800

    Initial boiler plate commit

---
+++ project.py
@@ -0,0 +1,16 @@
+#################
+#Header Shtuff 
+#################
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+from PIL import Image
+import json
+import requests
+
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def home():
+  return render_template ('index.html')
