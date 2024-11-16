f20357db037ca668f662985cee9f3e6ad54397ca
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2024-11-15 18:39:31 -0800

    Small change

--- project.py
+++ project.py
@@ -14,9 +14,3 @@ bootstrap = Bootstrap5(app)
 @app.route('/')
 def home():
   return render_template ('index.html')
-
-
-@app.route('/about')
-def about():
-  return render_template ('about.html')
-
