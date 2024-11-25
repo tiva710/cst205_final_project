4c5882ae95a6b9afaf3dcf96f53109caaf930d89
Tiva Rocco <143124479+tiva710@users.noreply.github.com>
2024-11-25 00:07:34 -0800

    Instructions, Hint Setup
    - Finished up the instructions/formatting
    - Added a route to toggle the hint on and off. As of right now the link is just a placeholder, but can easily be switched out with the path to the stored image.
    
    NOTE: I just now noticed Sebastian also added some hint functionality, so we can talk about which logic will work better for our plans/which code we want to keep. I just wanted to include mine in this commit since I made changes to the code

--- project.py
+++ project.py
@@ -2,16 +2,43 @@
 #Header Shtuff 
 #test comment 
 #################
-from flask import Flask, render_template
+from flask import Flask, render_template, request, url_for, redirect
 from flask_bootstrap import Bootstrap5
 from PIL import Image
 import json
 import requests
 
+#I am assuming we will have the image generation be done once we initially get the 
+#'correct' player for that round. Then we could save the image in the same place and have 
+#the same route for it everytime for the hint_image variable... right? 
+
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
 
+hint_showing = False
+
 @app.route('/')
-def home():
-  return render_template ('index.html')
+def home(): 
+   #IMAGE SRC HERE 
+  global hint_showing
+  if hint_showing == True: 
+    hint_image = 'https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png' 
+  else:
+     hint_image = None
+  return render_template ('index.html', hint_image=hint_image)
+
+
+@app.route('/show_hint', methods=['POST'])
+def show_hint():
+  global hint_showing
+
+  hint_showing = not hint_showing
+
+  return redirect(url_for('home'))
+ 
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
