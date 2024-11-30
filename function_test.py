4f91e8536d9a63b5be0bd5fa75a902832a7efd8b
Kevin <kcrapo@csumb.edu>
2024-11-30 14:44:53 -0800

    Function for downloading image from URL/ Prototype for downloading the image and doing silhouette at the same time.
    I researched methods for using requests to download image URL's and created a function to do so. Additionally, I researched how to download the image and open as a pillow image for manipulation and created a function prototype for integration with silhouette. At this junction I see two different ways we could go about accomplishing this task and would like to discuss the best way to do this with the group!

--- function_test.py
+++ function_test.py
@@ -1,5 +1,7 @@
 import requests
+import io
 from PIL import Image
+from project import silohouette
 
 # We ran into an issue where the images provided by the api were given as URL's and needed
 # to find a way to convert these URL images into pillow images for our silhouette function.
@@ -8,11 +10,50 @@ from PIL import Image
 # https://janakiev.com/blog/python-pilow-download-image/ 
 # https://requests.readthedocs.io/en/latest/user/advanced/#body-content-workflow
 
+#This function simply uses requests to download an image from a specified url
+# Saves the image to specified file path (I could modify to make our static/image folder default)
+def download_image(url, filepath):
+    try:
+        r = requests.get(url)
 
-filepath = 
-url = "https://images.unsplash.com/photo-1465056836041-7f43ac27dcb5?w=720"
+        # Check if the request was successful
+        if r.status_code == 200:
+            
+            # Save the image content to filepath
+            with open(filepath, 'wb') as f: 
+                f.write(r.content)
+                
+    except:
+        print("Error finding image")
+        
 
-r = requests.get(url)
-if r.status_code == 200:
-    with open(filepath, 'wb') as f:
-        f.write(r.content)
+
+#Prototype function for downloading image, opening as a pillow object, and doing silhouette at the same time       
+def download_and_save_pillow_image(url, filepath):
+    try:
+        # Send HTTP request to get the image content in stream mode
+        r = requests.get(url, stream=True)
+
+        # Check if the request was successful
+        if r.status_code == 200:
+            
+            # Open the image with pillow using io byte content
+            # https://janakiev.com/blog/python-pilow-download-image/ 
+            # https://requests.readthedocs.io/en/latest/user/advanced/#body-content-workflow    
+            img = Image.open(io.BytesIO(r.content))
+            
+            #------------- silhouette functionality here I think --------------
+            
+            img.save(filepath)
+    
+    except:
+        print("Error finding image")
+
+# Testing download image
+filepath = '/Users/kevincrapo/Documents/GitHub/cst205_final_project/static/images/image.jpg'
+url = 'https://media.api-sports.io/football/players/154.png'
+
+download_image(url=url,filepath=filepath)
+
+
+
